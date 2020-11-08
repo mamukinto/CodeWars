67bed019df05a8c9257d19ccfe5700dc6ca01fbd
Mamuka Sarkisyan <mamukinto2@gmail.com>
2020-11-08 22:59:02 +0400

    DescendingOrder kata completed

--- src/descedningorder/DescendingOrder.java
+++ src/descedningorder/DescendingOrder.java
@@ -1,8 +1,43 @@
 package descedningorder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class DescendingOrder {
     public static int sortDesc(final int num) {
+        // variables
+        List<Integer> numbers = new ArrayList<>();
+        int tempNum = num;
+        int result = 0;
+        int length = 0;
+        long temp = 1;
 
-            return 0;
+        //number of digits in num
+        while (temp <= num) {
+            length++;
+            temp *= 10;
+        }
+
+        // from int to list
+        for (int i = 0; i < length; i++) {
+            numbers.add(tempNum%10);
+            tempNum = tempNum/10;
+        }
+
+
+        // sort
+        numbers.sort(Collections.reverseOrder());
+
+        // from list to int
+        for (Integer integer : numbers) {
+            result = result * 10 + integer;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sortDesc(1012));
     }
 }
--- src/descedningorder/DescendingOrderTest.java
+++ src/descedningorder/DescendingOrderTest.java
@@ -1,4 +1,23 @@
 package descedningorder;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class DescendingOrderTest {
+    @Test
+    public void test_01() {
+        assertEquals(0, DescendingOrder.sortDesc(0));
+    }
+
+    @Test
+    public void test_02() {
+        assertEquals(2110, DescendingOrder.sortDesc(1012));
+    }
+
+
+    @Test
+    public void test_03() {
+        assertEquals(987654321, DescendingOrder.sortDesc(123456789));
+    }
 }
