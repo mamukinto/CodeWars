f3fd7156d5362fa12c0d4dc1b5fa28652d4a0079
Mamuka Sarkisyan <mamukinto2@gmail.com>
2020-11-08 21:10:28 +0400

    init commit

---
+++ src/descedningorder/DescendingOrder.java
@@ -0,0 +1,8 @@
+package descedningorder;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+
+            return 0;
+    }
+}
---
+++ src/descedningorder/DescendingOrderTest.java
@@ -0,0 +1,4 @@
+package descedningorder;
+
+public class DescendingOrderTest {
+}
